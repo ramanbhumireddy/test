a3187cee6848e2aea8e92ff54b9c047af55bbc6e
rabhumir <rabhumir@DIN66009480.corp.capgemini.com>
2017-06-14 15:54:40 +0530

    NSite Name change

--- nsite/src/main/java/org/nsite/NSiteApplication.java
+++ nsite/src/main/java/org/nsite/NSiteApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NsiteApplication {
+public class NSiteApplication {
 	
-	protected NsiteApplication(){}
+	protected NSiteApplication(){}
 
 	public static void main(String[] args) {
-		SpringApplication.run(NsiteApplication.class, args);
+		SpringApplication.run(NSiteApplication.class, args);
 	}
 }
